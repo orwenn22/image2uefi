9a9a67085c80cbaee21dbf36899b018e7a28f12b
orwenn22 <46846090+orwenn22@users.noreply.github.com>
2023-11-15 17:53:05 +0100

    export w and h

--- main.cpp
+++ main.cpp
@@ -11,8 +11,8 @@ int main(int argc, const char *argv[]) {
     Image input = LoadImage(argv[1]);
     if(input.data == nullptr) return 1;
 
-    FILE *output = fopen("out.h", "w");
-    fprintf(output, "unsigned char out[] = {");
+    FILE *output = fopen("background_image.h", "w");
+    fprintf(output, "unsigned char bg_buf[] = {");
 
     for(int y = 0; y < input.height; y++) {
         for(int x = 0; x < input.width; x++) {
@@ -21,8 +21,9 @@ int main(int argc, const char *argv[]) {
         }
     }
 
-
     fprintf(output, "};\n");
+    fprintf(output, "int bg_w = %i;\n", input.width);
+    fprintf(output, "int bg_h = %i;\n", input.height);
     fclose(output);
     UnloadImage(input);
 
